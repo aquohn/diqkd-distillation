0124fea5d7c8858acd90bf30f1c90ad7d3635562
John Khoo <john_khoo@u.nus.edu>
2022-01-24 14:51:12 +0800

    Complete code for brute-force wiring search with no-retrocausation

--- code/DIQKDWiring.jl/src/wiring.jl
+++ code/DIQKDWiring.jl/src/wiring.jl
@@ -25,14 +25,88 @@ includet("helpers.jl")
 includet("nonlocality.jl")
 
 # %%
-# Counting wirings
+# Counting and generating wirings
+function incr_idx_vec!(minvec::AbstractVector{T}, maxvec::AbstractVector{T}, vec::AbstractVector{T}) where {T <: Integer}
+  if !(length(minvec) == length(maxvec) == length(vec))
+    throw(ArgumentError("Vectors must be of the same length! Got minvec of $(length(minvec)), maxvec of $(length(maxvec)), and vec of $(length(vec))."))
+  end
+  for idx in Iterators.reverse(eachindex(vec))
+    if vec[idx] < maxvec[idx]
+      vec[idx] += 1
+      break
+    end
+    vec[idx] = minvec[idx]
+  end
+  # manually compare vec[1] before and after to detect overflow
+end
 
 num_lds(c, sett::Setting) = num_lds(c, sett.oA, sett.iA) * num_lds(c, sett.oB, sett.iB)
 num_lds(c, o, i) = (o * i^c)^(i^c * o)
-num_wirings(c, sett::Setting) = num_wirings(c, sett.oA, sett.iA) * num_wirings(c, sett.oB, sett.iB)
-num_wirings(c, o, i) = o^(i * o^c) * prod([i^(i * o^(j-1)) for j in 1:c])
-num_wirings_fix(c, sett::Setting, fA, fB) = num_wirings_fix(c, sett.oA, sett.iA, fA) * num_wirings_fix(c, sett.oB, sett.iB, fB)
-function num_wirings_fix(c, o, i, f)
+struct LDWiringIter{T <: Integer}
+  c::T
+  o::T
+  i::T
+  tmin::Vector{T}
+  tmax::Vector{T}
+  smin::Vector{T}
+  smax::Vector{T}
+  function LDWiringIter(c::Integer, o::Integer, i::Integer)
+    T = promote_type(typeof(c), typeof(o), typeof(i))
+    new{T}(c, o, i, repeat([1], c+1), vcat(repeat([i], c), o), repeat([1], c+1), vcat(i, repeat([o], c)))
+  end
+end
+function ldwiring_check_prob(ldwit::LDWiringIter{Ti}, chi::Array{Tp}) where {Tp <: Real, Ti <: Integer}
+  valid = true
+  c, o, i = ldwit.c, ldwit.o, ldwit.i
+  for j in 1:c
+    println(chi)
+    rmdims = (1+j+1):(1+c)
+    chij = dropdims(sum(chi; dims=rmdims); dims=Tuple(rmdims))
+    for t in itprod(1:o, repeat([1:i], j)...)
+      for scomm in itprod(repeat([1:o], j-1)...)
+        for x in 1:i
+          probs = (chij[t..., scomm..., svar..., x]
+                   for svar in itprod(repeat([1:o], c-(j-1))...))
+          if !(all(p -> p == first(probs), probs))
+            valid = false
+            @goto checkdone
+          end
+        end
+      end
+    end
+  end
+  @label checkdone
+  return valid
+end
+Base.iterate(ldwit::LDWiringIter) = iterate(ldwit, nothing)
+function Base.iterate(ldwit::LDWiringIter{T}, initstate) where T
+  c, o, i = ldwit.c, ldwit.o, ldwit.i
+  ts = (t for t in itprod(1:o, repeat([1:i], c)...))  # diff targets
+  ss = (s for s in itprod(repeat([1:o], c)..., 1:i))  # diff sources
+  lenss = length(ss)  # number of diff sources
+  dtsit = itprod(repeat([ts], lenss)...)  # iterator over possible deterministic targets
+  if isnothing(initstate)
+    dtsitval = iterate(dtsit)
+  else
+    dtsitval = iterate(dtsit, initstate)
+  end
+  while !isnothing(dtsitval)
+    dts, state = dtsitval
+    chi = zeros(T, o, repeat([i], c)..., i, repeat([o], c)...)
+    for (dt, s) in zip(dts, ss)
+      chi[dt..., s...] = 1
+    end
+    if ldwiring_check_prob(ldwit, chi)
+      return chi, state
+    end
+    dtsitval = iterate(dtsit, state)
+  end
+end
+
+num_wiring_maps(c, sett::Setting) = num_wiring_maps(c, sett.oA, sett.iA) * num_wiring_maps(c, sett.oB, sett.iB)
+num_wiring_maps(c, o, i) = o^(i * o^c) * prod([i^(i * o^(j-1)) for j in 1:c])
+num_wiring_maps_fix(c, sett::Setting, fA, fB) = num_wiring_maps_fix(c, sett.oA, sett.iA, fA) * num_wiring_maps_fix(c, sett.oB, sett.iB, fB)
+function num_wiring_maps_fix(c, o, i, f)
   if f > i
     throw(ArgumentError("$f inputs to fix, but there are only $i inputs!"))
   end
